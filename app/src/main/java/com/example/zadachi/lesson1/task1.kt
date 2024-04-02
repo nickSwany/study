7789360f61fc02fae6d87f29856986ca182dbfdf
nickSwany <Thief.leroy@yandex.ru>
2024-04-03 02:03:00 +0300

    Урок 1, Задача 1

--- app/src/main/java/com/example/zadachi/lesson1/task1.kt
+++ app/src/main/java/com/example/zadachi/lesson1/task1.kt
@@ -1,6 +1,10 @@
 package com.example.zadachi.lesson1
 
 fun main() {
+
     val onlineOdering: Int = 75
     val letterOfThanks: String = "Благодарим вас за покупку в нашем в магазине!"
+
+    println(onlineOdering)
+    println(letterOfThanks)
 }
