00babcb8d75797035a78b2364b0ecacdd5f44d8e
nickSwany <Thief.leroy@yandex.ru>
2024-04-03 02:09:30 +0300

    Урок 1, Задача 2

---
+++ app/src/main/java/com/example/zadachi/lesson1/Task2.kt
@@ -0,0 +1,18 @@
+package com.example.zadachi.lesson1
+
+fun main() {
+
+    val onlineOdering = 75
+    val letterOfThanks = "Благодарим вас за покупку в нашем в магазине!"
+
+    println(onlineOdering)
+    println(letterOfThanks)
+
+    var currentWorksPeople = 2000
+
+    //println(currentWorksPeople)
+
+    currentWorksPeople -= 1
+
+    println(currentWorksPeople)
+}
